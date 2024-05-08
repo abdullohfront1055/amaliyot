45b4e5aef7f7c90ea01ed95d608b3c6a7c4b7b02
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-08 11:42:08 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,65 @@
+// 1 - vazifa
+//  let son1 = Number(prompt("Birinchi son1 kiriting"));
+//  let son2 = Number(prompt("Ikkinchi sonn2 kiriting"));
+//  let natija = son1 + son2;
+
+//  alert(son1 + son2);
+
+
+// 2 - vazifa
+// let a = '12'
+// let b = Number(a);
+
+// console.log(let b = Number(a));
+
+
+// 3 - vazifa
+// let taxminiyson = Math.floor(Math.random() * 100);
+
+// if(taxminiyson % 2 === 0) {
+//     console.log(taxminiyson + " - juft son")
+// } else {
+//     console.log( taxminiyson + " - toq son")
+// }
+
+
+// 4- vazifa
+// prompt("Birinchi sozni kiriting");
+
+// let soz = prompt("");
+// if (soz === "Mars") {
+//    console.log("Mars")
+// } else {
+//     console.log("Mars IT school")
+// }
+
+
+// 5 - vazifa
+
+// let ismlar = ["mashina", "telefon", "Abduloh", "Mars", "pepsi"];
+// let natija = [];
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(ismlar[i]);
+//     if (ismlar[i].length > 5) {
+//         natija.push(ismlar[i]);
+//     }
+// }
+// console.log(natija);
+
+// 6 - vazifa
+
+// let juft = [];
+// let toq = [];
+
+// for (i = 0; i < 100; i++) {
+//     if (i % 2 === 0) {
+//         juft.push(i)
+//     } else {
+//         toq.push(i)
+//     }
+// }
+
+// console.log(toq);
+// console.log(juft);
+
